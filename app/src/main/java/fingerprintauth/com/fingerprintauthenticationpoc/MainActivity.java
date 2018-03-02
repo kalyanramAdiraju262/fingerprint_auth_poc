c5b8d948665c5eda6ceb40f4b9f163d79d3ce39c
KalyanramAdiraju262 <KalyanramAdiraju262@gmail.com>
2018-03-02 23:30:12 +0530

    Test commit

--- app/src/main/java/fingerprintauth/com/fingerprintauthenticationpoc/MainActivity.java
+++ app/src/main/java/fingerprintauth/com/fingerprintauthenticationpoc/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -37,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Log.d("hello1","1"
+
+        );
 
     }
 }
